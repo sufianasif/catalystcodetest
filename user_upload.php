635057bd2a4f010754b550f3d00ad25dc89290ce
Sufian Asif <sufianasif@Sufians-MacBook-Pro.local>
2020-09-22 19:10:00 +1000

    forth commit

--- user_upload.php
+++ user_upload.php
@@ -5,7 +5,7 @@ if ($argv[0] == "user_upload.php" && $argv[1] == null) {
     echo " Please run --help for Directives List";
 } else if ($argv[1] == "--help") {
     directivesDisplay();
-} else if($argv[1] == "--create_table") {
+} else if ($argv[1] == "--create_table") {
     $data = file_get_contents("env.json");
     $json = json_decode($data, true);
     foreach ($json as $input => $input_value) {
@@ -15,13 +15,38 @@ if ($argv[0] == "user_upload.php" && $argv[1] == null) {
             break;
         }
     }
-        if($bool == 0 ){
-            $connect = createConnection();
-    createDbTable($connect); }
-    
-}else{
-    echo "Not a valid Command";
+    if ($bool == 0) {
+        $connect = createConnection();
+        createDbTable($connect);
+    }
+} else if ($argv[1] == "-u" && !empty($argv[2])) {
+    writeJson("User", $argv[2]);
+} else if ($argv[1] == "-p" && !empty($argv[2])) {
+    writeJson("Password", $argv[2]);
+} else if ($argv[1] == "-h" && !empty($argv[2])) {
+    writeJson("Host", $argv[2]);
+} else if ($argv[1] == "-db" && !empty($argv[2])) {
+    writeJson("Database", $argv[2]);
+} else if ($argv[1] == "-port" && !empty($argv[2])) {
+    writeJson("Port", $argv[2]);
+} else {
+    echo "Not a valid Command \n";
 }
+
+//fucntion takes the Mysql env data from command line input and writes in the env.json file
+function writeJson($datakey, $datavalue)
+{
+    $data = file_get_contents("env.json");
+    $json = json_decode($data, true);
+
+    $json[$datakey] = $datavalue;
+    $fh = fopen("env.json", 'w')
+    or die("" . $datakey . "not set.Error opening output file");
+    fwrite($fh, json_encode($json, JSON_UNESCAPED_UNICODE));
+    echo ("" .$datakey . " is set.");
+    fclose($fh);
+}
+
 //function displays all the valid directives
 function directivesDisplay()
 {
@@ -33,7 +58,7 @@ function directivesDisplay()
         "-u - MySQL username",
         "-p - MySQL password",
         "-h - MySQL host",
-        ];
+    ];
     foreach ($arrayofdirectives as $result) {
         echo $result . "\n";
     }
